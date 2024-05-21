d338cafef03f77b869f02c719b43e5bde0cc8843
juanbburillo <juanburillodev@gmail.com>
2024-05-21 16:10:48 +0200

    create expenses service

---
+++ src/app/core/services/expenses/expenses.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpensesService {
+  private url = '/api/expense';
+
+  constructor(private http: HttpClient) { }
+
+  getExpenses() {
+    return this.http.get(this.url);
+  }
+}

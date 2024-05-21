9f44d933b19cd5771b4428b1ca8f82848dcc4c0a
juanbburillo <juanburillodev@gmail.com>
2024-05-21 16:11:04 +0200

    create list-expenses component

--- src/app/expenses/list-expenses/list-expenses.component.ts
+++ src/app/expenses/list-expenses/list-expenses.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExpensesService } from '../../core/services/expenses/expenses.service';
 
 @Component({
   selector: 'app-list-expenses',
   standalone: true,
   imports: [],
   templateUrl: './list-expenses.component.html',
-  styles: ``
 })
-export class ListExpensesComponent {
+export class ListExpensesComponent implements OnInit {
+  data: any;
 
+  constructor(private expenseService: ExpensesService) {}
+
+  ngOnInit() {
+    this.showExpenses();
+  }
+
+  showExpenses() {
+    this.expenseService.getExpenses().subscribe((response) => {
+      this.data = response;
+    });
+  }
+
+  timeAgo(dateString: string) {
+    const inputDate: any = new Date(dateString);
+    const now: any = new Date();
+    const diff = now - inputDate; // Difference in milliseconds
+
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30); // Approximation
+    const years = Math.floor(months / 12);
+
+    if (years > 0) {
+      return years === 1 ? '1 year ago' : `${years} years ago`;
+    } else if (months > 0) {
+      return months === 1 ? '1 month ago' : `${months} months ago`;
+    } else if (days > 0) {
+      return days === 1 ? '1 day ago' : `${days} days ago`;
+    } else if (hours > 0) {
+      return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
+    } else if (minutes > 0) {
+      return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
+    } else {
+      return seconds === 1 ? '1 second ago' : `${seconds} seconds ago`;
+    }
+  }
 }

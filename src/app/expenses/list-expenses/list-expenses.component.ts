0a7f611037fd89b4364e8d92f9b38ac00e5ad947
juanbburillo <juanburillodev@gmail.com>
2024-05-21 15:08:00 +0200

    create list-expenses component

---
+++ src/app/expenses/list-expenses/list-expenses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-expenses',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-expenses.component.html',
+  styles: ``
+})
+export class ListExpensesComponent {
+
+}

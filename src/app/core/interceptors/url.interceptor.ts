8e8286e76db39d276a5bd176626ad4d08193aaa1
juanbburillo <juanburillodev@gmail.com>
2024-05-28 12:08:02 +0200

    change 'enviroment' for 'environment'

--- src/app/core/interceptors/url.interceptor.ts
+++ src/app/core/interceptors/url.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
-import { enviroment } from "../../../enviroment";
+import { environment } from "../../../environments/environment";
 
 export const urlInterceptor: HttpInterceptorFn = (request: HttpRequest<unknown>, next: HttpHandlerFn) => {
-    const requestClone = request.clone({url: enviroment.apiUrl + request.url});
+    const requestClone = request.clone({url: environment.apiUrl + request.url});
     return next(requestClone);
 }
